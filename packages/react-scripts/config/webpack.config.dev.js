aa5f3c5115f78b2bc7499a04ce6a5347ab0636d5
Oscar Eckbo <oscareckbo@users.noreply.github.com>
2018-06-06 15:23:10 +0200

    Using contextpath for webpackresources

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -24,11 +24,11 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
-const publicPath = '/';
+const publicPath = paths.publicUrl || '/';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = '';
+const publicUrl = (paths.publicUrl && paths.publicUrl.charAt(0) === '/' ? paths.publicUrl.slice(1) : paths.publicUrl) || '';
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
 
