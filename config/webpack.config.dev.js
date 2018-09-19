baf377c3be4bc7d86cceefd66c71d0badaf9bd47
alexander-svendsen <alexandersvendsen1990@gmail.com>
2018-09-19 21:30:14 +0200

    Updated autoprefixer, and enabled grid-support

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -213,6 +213,7 @@ module.exports = {
                         'Firefox ESR',
                         'not ie < 9', // React doesn't support IE8 anyway
                       ],
+                      grid: true,
                       flexbox: 'no-2009',
                     }),
                   ],
@@ -251,6 +252,7 @@ module.exports = {
                         'Firefox ESR',
                         'not ie < 9', // React doesn't support IE8 anyway
                       ],
+                      grid: true,
                       flexbox: 'no-2009',
                     }),
                   ],
@@ -261,69 +263,6 @@ module.exports = {
                 options: env.raw.REACT_APP_LESS_VARS,
               },
             ],
-          },
-          // The notation here is somewhat confusing.
-          // "postcss" loader applies autoprefixer to our CSS.
-          // "css" loader resolves paths in CSS and adds assets as dependencies.
-          // "style" loader normally turns CSS into JS modules injecting <style>,
-          // but unlike in development configuration, we do something different.
-          // `ExtractTextPlugin` first applies the "postcss" and "css" loaders
-          // (second argument), then grabs the result CSS and puts it into a
-          // separate file in our build process. This way we actually ship
-          // a single CSS file in production instead of JS code injecting <style>
-          // tags. If you use code splitting, however, any async bundles will still
-          // use the "style" loader inside the async code so CSS from them won't be
-          // in the main CSS file.
-          {
-            test: /\.less$/,
-            loader: ExtractTextPlugin.extract(
-              Object.assign(
-                {
-                  fallback: {
-                    loader: require.resolve('style-loader'),
-                    options: {
-                      hmr: false,
-                    },
-                  },
-                  use: [
-                    {
-                      loader: require.resolve('css-loader'),
-                      options: {
-                        importLoaders: 1,
-                        minimize: true,
-                        sourceMap: shouldUseSourceMap,
-                      },
-                    },
-                    {
-                      loader: require.resolve('postcss-loader'),
-                      options: {
-                        // Necessary for external CSS imports to work
-                        // https://github.com/facebookincubator/create-react-app/issues/2677
-                        ident: 'postcss',
-                        plugins: () => [
-                          require('postcss-flexbugs-fixes'),
-                          autoprefixer({
-                            browsers: [
-                              '>1%',
-                              'last 4 versions',
-                              'Firefox ESR',
-                              'not ie < 9', // React doesn't support IE8 anyway
-                            ],
-                            flexbox: 'no-2009',
-                          }),
-                        ],
-                      },
-                    },
-                    {
-                      loader: require.resolve('less-loader'),
-                      options: env.raw.REACT_APP_LESS_VARS,
-                    },
-                  ],
-                },
-                extractTextPluginOptions
-              )
-            ),
-            // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
